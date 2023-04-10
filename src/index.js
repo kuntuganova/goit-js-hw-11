73b3a5ddbc6c2d4ab7c9ea356f231077a6860baa
kuntuganova <elya.kuntuganova@gmail.com>
2023-04-10 21:21:50 +0600

    import / export

--- src/index.js
+++ src/index.js
@@ -1,205 +1,115 @@
 import './css/styles.css';
-import axios from 'axios';
-import Notiflix from 'notiflix';
-const debounce = require('lodash.debounce');
+import NewsApiService from './js/api-service';
+import { lightbox } from './js/lightbox';
+import { Notiflix } from 'notiflix';
 
-const searchForm = document.querySelector('.search-form');
-const submitBtn = document.querySelector("[type='submit']");
-const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
-const footer = document.querySelector('.footer');
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  galleryContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+let isShown = 0;
+const newsApiService = new NewsApiService();
 
-const PIXABAY_URL = 'https://pixabay.com/api/';
-const PIXABAY_API_KEY = '35190639-3a7acd02227a3d19c5cf9cfef';
-const DEBOUNCE_DELAY = 100;
-let currentPage = 1;
-const POST_PER_PAGE = 40;
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-searchForm.addEventListener(
-  'input',
-  debounce(e => {
-    const searchValue = e.target.value;
-    localStorage.setItem('search-term', searchValue.trim());
-  }, DEBOUNCE_DELAY)
-);
+const options = {
+  rootMargin: '50px',
+  root: null,
+  threshold: 0.3,
+};
+const observer = new IntersectionObserver(onLoadMore, options);
 
-submitBtn.addEventListener('click', e => {
-  e.preventDefault();
-  const savedSearch = localStorage.getItem('search-term');
-  if (savedSearch === null || savedSearch === '') {
-    Notiflix.Notify.info('Please type something in the search input.');
+function onSearch(element) {
+  element.preventDefault();
+
+  refs.galleryContainer.innerHTML = '';
+  newsApiService.query =
+    element.currentTarget.elements.searchQuery.value.trim();
+  newsApiService.resetPage();
+
+  if (newsApiService.query === '') {
+    Notify.warning('Please, fill the main field');
     return;
   }
-  currentPage = 1;
-  fetchImages(savedSearch, currentPage);
-});
 
-const fetchImages = async (searchValue, currentPage) => {
-  let galleryMarkup = '';
-  let params = new URLSearchParams({
-    key: PIXABAY_API_KEY,
-    q: searchValue,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: POST_PER_PAGE,
-  });
+  isShown = 0;
+  fetchGallery();
+  onRenderGallery(hits);
+}
 
-  try {
-    const response = await axios.get(
-      `${PIXABAY_URL}?${params}&page=${currentPage}`
+function onLoadMore() {
+  newsApiService.incrementPage();
+  fetchGallery();
+}
+
+async function fetchGallery() {
+  refs.loadMoreBtn.classList.add('is-hidden');
+
+  const result = await newsApiService.fetchGallery();
+  const { hits, total } = result;
+  isShown += hits.length;
+
+  if (!hits.length) {
+    Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`
     );
-    // console.log('response: ', response);
-    const imagesArray = response.data.hits;
-    // console.log('imagesArray: ', imagesArray);
-    if (searchValue === '') {
-    }
-    if (imagesArray.length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-
-      return;
-    }
-
-    loadMoreBtn.style.display = 'block';
-    footer.style.display = 'flex';
-
-    galleryMarkup = imagesArray
-      .map(image => {
-        return `
-        <div class="photo-card">
-          <div class ="thumb">
-            <img class="img" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-          </div>
-          <div class="info">
-            <p class="info-item">
-              <b>Likes</b>
-              ${image.likes}
-            </p>
-            <p class="info-item">
-              <b>Views</b>
-              ${image.views}
-            </p>
-            <p class="info-item">
-              <b>Comments</b>
-              ${image.comments}
-            </p>
-            <p class="info-item">
-              <b>Downloads</b>
-              ${image.downloads}
-            </p>
-          </div>
-        </div>
-      `;
-      })
-      .join('');
-
-    gallery.innerHTML = galleryMarkup;
-  } catch (error) {
-    console.error(error);
+    refs.loadMoreBtn.classList.add('is-hidden');
+    return;
   }
-};
 
-const fetchNewImages = async (searchValue, currentPage) => {
-  let galleryMarkup = '';
-  let params = new URLSearchParams({
-    key: PIXABAY_API_KEY,
-    q: searchValue,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: POST_PER_PAGE,
-  });
+  onRenderGallery(hits);
+  isShown += hits.length;
 
-  try {
-    const response = await axios.get(
-      `${PIXABAY_URL}?${params}&page=${currentPage}`
-    );
-    // console.log('response: ', response);
-    const imagesArray = response.data.hits;
-    // console.log('imagesArray: ', imagesArray);
-    if (imagesArray.length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      return;
-    }
-
-    galleryMarkup = imagesArray
-      .map(image => {
-        return `
-        <div class="photo-card">
-          <div class ="thumb">
-            <img class="img" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-          </div>
-          <div class="info">
-            <p class="info-item">
-              <b>Likes</b>
-              ${image.likes}
-            </p>
-            <p class="info-item">
-              <b>Views</b>
-              ${image.views}
-            </p>
-            <p class="info-item">
-              <b>Comments</b>
-              ${image.comments}
-            </p>
-            <p class="info-item">
-              <b>Downloads</b>
-              ${image.downloads}
-            </p>
-          </div>
-        </div>
-      `;
-      })
-      .join('');
-
-    gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-  } catch (error) {
-    console.error(error);
+  if (isShown < total) {
+    Notify.success(`Hooray! We found ${total} images !!!`);
+    refs.loadMoreBtn.classList.remove('is-hidden');
   }
-};
 
-loadMoreBtn.addEventListener('click', async () => {
-  const searchValue = localStorage.getItem('search-term');
-  currentPage++;
-  let params = new URLSearchParams({
-    key: PIXABAY_API_KEY,
-    q: searchValue,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: POST_PER_PAGE,
-  });
-  try {
-    const response = await axios.get(
-      `${PIXABAY_URL}?${params}&page=${currentPage}`
-    );
-
-    const imagesArray = response.data.hits;
-    const imagesPerPage = POST_PER_PAGE;
-
-    const totalImages = response.data.totalHits;
-    const maxPageNumber = totalImages / imagesPerPage;
-    const maxPageNumberRoundUp = Math.ceil(maxPageNumber);
-    console.log('currentPage: ', currentPage);
-    console.log('maxPageNumber: ', maxPageNumber);
-    console.log('maxPageNumberRoundUp: ', maxPageNumberRoundUp);
-
-    if (currentPage === maxPageNumberRoundUp) {
-      footer.style.display = 'none';
-      loadMoreBtn.style.display = 'none';
-      Notiflix.Notify.info(
-        "We're sorry, but you've reached the end of search results."
-      );
-    }
-
-    fetchNewImages(searchValue, currentPage);
-  } catch (error) {
-    console.error(error);
+  if (isShown >= total) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
   }
-});
-footer.style.display = 'none';
-loadMoreBtn.style.display = 'none';
+}
+
+function onRenderGallery(elements) {
+  const markup = elements
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+      <a href="${largeImageURL}">
+        <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+      </a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          ${likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          ${views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          ${comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          ${downloads}
+        </p>
+      </div>
+      </div>`;
+      }
+    )
+    .join('');
+  refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
